6b36d25c2ad3f26c73db72258b8141ec571dc60a
ZacharyHeathcock <zacharyheathcock@gmail.com>
2024-11-05 19:56:07 -0600

    First working announcements page

---
+++ statuspage_application/announcements/urls.py
@@ -0,0 +1,7 @@
+# announcements/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.announcement_list, name='announcement_list'),
+]
--- statuspage_application/statuspage_application/settings.py
+++ statuspage_application/statuspage_application/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'announcements',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
